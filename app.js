78a10c026d886cc3f66e12350fa9b394c50115e7
Jorge Henrique Silva Saldanha <jorgehenriquesaldanha@gmail.com>
2021-06-28 19:43:41 -0300

    Páginas HTML sendo renderizadas com css

--- app.js
+++ app.js
@@ -9,6 +9,33 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
 
+app.use(express.static(__dirname + "/public"))
+app.set('view engine', 'ejs')
+
+app.get('/super_merc', function(req, res){
+  res.render("../views/inicio")
+})
+
+app.get('/historia', function(req, res){
+  res.render("../views/historia")
+})
+
+app.get('/produtos', function(req, res){
+  res.render("../views/produtos")
+})
+
+app.get('/contato', function(req, res){
+  res.render("../views/contato")
+})
+
+app.get('/clientes', function(req, res){
+  res.render("../views/clientes")
+})
+
+app.get('/adm', function(req, res){
+  res.render("../views/adm")
+})
+
 const authRota = require('./rotas/authRotas')
 app.use('/super_merc/auth', authRota)
 app.use(funcionarioController.validarToken)
